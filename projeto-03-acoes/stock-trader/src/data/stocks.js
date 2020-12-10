c2bff2ad8eb962fa63bbbf977397b91ad03a6343
Alexandre Souza <alexandre.souza@sharecare.com>
2020-12-10 01:43:02 -0300

    304. Configurando o Vuex no Projeto 305. Adicionando o Módulo Portfolio ao Vuex 306. Trabalhando com Ações do Portfolio 307. Conectando o Portfolio ao Vuex

---
+++ projeto-03-acoes/stock-trader/src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    {id: 1, name: 'ITSA4', price: 11.88},
+    {id: 2, name: 'XPML11', price: 109.15},
+    {id: 3, name: 'TAEE11', price: 33.21},
+    {id: 4, name: 'VIVT3', price: 46.65},
+
+]
