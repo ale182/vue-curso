2aafb3b5f31fc5b0e8b548b7a31f340dae3ad9b5
Alexandre Souza <alexandre.souza@sharecare.com>
2020-12-15 01:21:25 -0300

    Projeto #04 - Projeto Calculadora
    326. Criando Projeto
    327. Criando Componentes

---
+++ projeto-calculator/src/main/Calculator.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="calculator">
+    <Display></Display>
+    <Button></Button>
+  </div>
+</template>
+
+<script>
+import Button from "@/components/Button";
+import Display from "@/components/Display";
+
+export default {
+  name: "Calculator",
+  components: { Button , Display }
+}
+</script>
+
+<style scoped>
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
+
+  display: grid;
+  grid-template-columns: repeat(4,25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px;
+}
+</style>
